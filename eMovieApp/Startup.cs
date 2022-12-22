b5b6917ec6fe78dcc764382ebac608486c60813e
bardha cocaj <fc52610@ubt-uni.net>
2022-12-22 18:55:55 +0100

    model creations

--- eMovieApp/Startup.cs
+++ eMovieApp/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eMovieApp.Data;
 
 namespace eMovieApp
 {
@@ -23,6 +24,10 @@ namespace eMovieApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //Dbcontext configuration
+            services.AddDbContext<AppDbContext>();
+
             services.AddControllersWithViews();
         }
 
