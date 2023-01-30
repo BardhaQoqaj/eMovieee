ba7559d27d053e5df05d47ffcc9e278e6a7685c2
bardha cocaj <fc52610@ubt-uni.net>
2023-01-30 15:03:29 +0100

    Added order&OrderItems logic

---
+++ eMovieApp/Models/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eMovieApp.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public int MovieId { get; set; }
+        [ForeignKey("MovieId")]
+
+        public  Movie Movie { get; set; }
+
+        public int OrderID { get; set; }
+        [ForeignKey("OrderID")]
+
+        public Order Order { get; set; }
+    }
+}
