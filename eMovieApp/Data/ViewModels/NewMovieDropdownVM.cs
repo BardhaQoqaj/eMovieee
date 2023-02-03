0d9b7ce92c8885edcc7c3660fd04d3a87bf6db23
Endi <eh51545@ubt-uni.net>
2023-02-03 23:08:52 +0100

    new movie and movie relationships

---
+++ eMovieApp/Data/ViewModels/NewMovieDropdownVM.cs
@@ -0,0 +1,22 @@
+using eMovieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMovieApp.Data.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }
+
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
+}
